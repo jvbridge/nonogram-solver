5ab61f6837bd1fbbe7cb10001cad6058cf2a763e
Jonathan Bridge <jvbridge@gmail.com>
2022-06-13 11:07:05 -0700

    empty button grid

---
+++ src/components/box/Box.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+function Box({ color, state }) {
+  return <Button></Button>;
+}
+
+export default Box;
---
+++ src/components/grid/Grid.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Box from '../box/Box';
+
+function Grid(props) {
+  return (
+    <div className="container">
+      <div className="row">
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+      </div>
+      <div className="row">
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+      </div>
+      <div className="row">
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+      </div>
+      <div className="row">
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+      </div>
+      <div className="row">
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+      </div>
+    </div>
+  );
+}
+
+export default Grid;
