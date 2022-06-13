d3473c0d527ea46ab2dabc0d68068a085de96bf3
Jonathan Bridge <jvbridge@gmail.com>
2022-06-13 11:28:10 -0700

    added some basic rendering functions

--- src/components/grid/Grid.js
+++ src/components/grid/Grid.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import Box from '../box/Box';
 
+// rendering functions for the boxes
+const createBox = (state) => {
+  return <Box state="state" />;
+};
+
+const createRow = (stateArr) => {
+  const ret = [];
+  stateArr.forEach((state) => {
+    ret.push(createBox(state));
+  });
+  return <div className="row">{ret}</div>;
+};
+
+const createGrid = (stateArrs) => {
+  const ret = [];
+  stateArrs.forEach((row) => {
+    ret.push(createRow(row));
+  });
+  return <div className="container">{ret}</div>;
+};
+
 function Grid(props) {
   return (
     <div className="container">
